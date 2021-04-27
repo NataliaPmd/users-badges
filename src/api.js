842efd99e6a0177536c6b6be2c7535020be9ef1d
Natalia Pascual <natalia.pmd@gmail.com>
2021-04-27 11:17:57 +0200

    fix url

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://team-members-list.herokuapp.com/";
+const BASE_URL = "https://team-members-list.herokuapp.com";
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const randomNumber = (min = 0, max = 1) =>
